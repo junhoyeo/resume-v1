e1b9960546b88bd82db54d8d4715ed059adc0673
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-09-21 10:08:22 +0900

    chore: add deploy script

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import Vue from 'vue';
-import ElementUI from 'element-ui';
 import App from './App.vue';
-
-import 'element-ui/lib/theme-chalk/index.css';
-
-Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
